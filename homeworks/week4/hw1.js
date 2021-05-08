74ce8e62cf77bf584ed8ca734699aff458376af8
Peter Wang <wangpeter588@gmail.com>
2021-05-08 21:52:16 +0800

    20210508_Wangpoching_week4_hw1

---
+++ homeworks/week4/hw1.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+request(
+  'https://lidemy-book-store.herokuapp.com/books?_limit=10',
+  (error, response, body) => {
+    if (response.statusCode === 200) {
+      getbooks(JSON.parse(body))
+    }
+  }
+)
+
+function getbooks(body) {
+  for (let i = 0; i < body.length; i++) {
+    console.log(`${i + 1} ${body[i].name}`)
+  }
+}
