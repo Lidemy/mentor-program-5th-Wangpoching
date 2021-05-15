a9ca7bb1c9dbe896d8a54117726ee2f86ffd53ac
Peter Wang <wangpeter588@gmail.com>
2021-05-16 01:52:31 +0800

    week4_revised5

--- homeworks/week4/challenge.js
+++ homeworks/week4/challenge.js
@@ -1,5 +1,3 @@
-// 備註：這個程式碼還需要別的東西才能跑，因為 Twitch API 規則有變
-// 所以基本上只是讓大家看個概念而已，概念絕對是相通的，可以自己換成舊的 API 試試看
 const request = require('request')
 const process = require('process')
 
@@ -7,9 +5,6 @@ const Accept = 'application/vnd.twitchtv.v5+json'
 const Client = 'xkypa7ouwlhjupibscd4mk8lhwgtgk'
 const BASE_URL = 'https://api.twitch.tv/kraken'
 
-/*
-  callback 會根據參數回傳部分實況列表
-*/
 function getStreams(name, callback) {
   const url = `${BASE_URL}/streams/?game=${name}&limit=100&offset=0`
   request({
