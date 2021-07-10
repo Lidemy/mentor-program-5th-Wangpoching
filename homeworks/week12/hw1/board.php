cca1598242f17d223f7ba259304507a7925f0687
Peter Wang <wangpeter588@gmail.com>
2021-07-10 21:29:43 +0800

    0710_Wangpoching_HW12_homework_revised2

--- homeworks/week12/hw1/board.php
+++ homeworks/week12/hw1/board.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <script src="index.js"></script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link href="modal.css" rel="stylesheet">
@@ -29,16 +30,16 @@
       </form>
       <div class="comments row justify-content-center mt-5">
       </div>
-    </div>
-    <div class="container fixed-bottom d-flex flex-row-reverse bg-dark">
-      <div class="dropup">
-        <a class="btn btn-dark dropdown-toggle" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false"></a>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-          <li><a class="dropdown-item btn-gobottom">移至最底</a></li>
-          <li><a class="dropdown-item btn-gotop">回到頂部</a></li>
-        </ul>
+      <div class="container mt-auto d-flex flex-row-reverse bg-dark">
+        <div class="dropup">
+          <a class="btn btn-dark dropdown-toggle" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false"></a>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <li><a class="dropdown-item btn-gobottom">移至最底</a></li>
+            <li><a class="dropdown-item btn-gotop">回到頂部</a></li>
+          </ul>
+        </div>
       </div>
     </div>
+
   </body>
-  <script src="index.js"></script>
 </html>
