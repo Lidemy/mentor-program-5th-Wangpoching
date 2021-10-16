5e9d5b623f83e03775d882a29c5452cbb832e37a
Peter Wang <wangpeter588@gmail.com>
2021-10-16 12:01:43 +0800

    20211016_fix_merge

--- homeworks/week17/hw2/public/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-function focusinRemoveErrmsg(delegation, errmsg) {
-  delegation.addEventListener('focusin', () => {
-    if (!errmsg.classList.contains('hidden')) {
-      errmsg.classList.add('hidden')
-    }
-  })
-}
