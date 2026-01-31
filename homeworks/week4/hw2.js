ba3573e954b41cb3593f09b60fe471cdbaf682fc
Peter <peter@cacdi.com>
2026-02-01 00:42:09 +0800

    week4 hw update

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,188 +1,126 @@
-const BASE_URL = 'https://lidemy-book-store.herokuapp.com'
-const request = require('request')
+/*
+閱讀開頭給的 API 文件並串接，用 node.js 寫出一個程式並接受參數，輸出相對應的結果，範例如下：
+
+node hw2.js list // 印出前二十本書的 id 與書名
+node hw2.js read 1 // 輸出 id 為 1 的書籍
+node hw2.js delete 1 // 刪除 id 為 1 的書籍
+node hw2.js create "I love coding" // 新增一本名為 I love coding 的書
+node hw2.js update 1 "new name" // 更新 id 為 1 的書名為 new name
+*/
 const process = require('process')
+const request = require('request')
+const queryString = require('query-string')
 
-function getBooks(n) {
-  request(`${BASE_URL}/books?_limit=${n}`, (error, response, body) => {
-    if (error) {
-      console.log(error)
-      return
+const BASE_URL = 'https://lidemy-book-store.herokuapp.com'
+
+// 將 request 的處理統一包裝到 call 裡面, callback 給予 err 以及 JSON.parse(body)
+const call = (options, callback) => {
+  const queryParams = options.query ? `?${queryString.stringify(options.query)}` : ''
+  request[options.method]({
+    url: `${BASE_URL}${options.path}${queryParams}`,
+    ...(options.body && { form: options.body })
+  }, (err, res, body) => {
+    if (err) {
+      return callback(err)
     }
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      let books
+
+    if (res.statusCode >= 200 && res.statusCode < 300) {
       try {
-        books = JSON.parse(body)
-      } catch (error) {
-        console.log(error)
-        return
+        const content = JSON.parse(body)
+        return callback(null, content)
+      } catch (parseErr) {
+        return callback(parseErr)
       }
-      for (const value of books) {
-        console.log(`${value.id} ${value.name}`)
-      }
-      return
     }
-    console.log('something went wrong')
+
+    if (res.statusCode === 404) {
+      return callback(new Error('查無此書'))
+    }
+
+    callback(new Error('發生問題'))
   })
 }
 
-function getBook(id) {
-  request(`${BASE_URL}/books/${id}`, (error, response, body) => {
-    if (error) {
-      console.log(error)
-      return
-    }
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      let book
-      try {
-        book = JSON.parse(body)
-        console.log(`${book.id} ${book.name}`)
-        return
-      } catch (error) {
-        console.log(error)
-        return
-      }
-    }
-    if (response.statusCode === 404) {
-      let parsedBody
-      try {
-        parsedBody = JSON.parse(body)
-        if (!Object.keys(parsedBody).length) {
-          console.log('查無此書')
-          return
-        }
-      } catch (error) {
-      }
-      console.log('發生問題')
-      return
-    }
-    console.log('發生問題')
+// 取得前 n 本書籍
+const getBooks = (n) => {
+  call({
+    method: 'get',
+    path: '/books',
+    query: { _limit: n }
+  }, (err, books) => {
+    if (err) return console.log(err.message)
+    books.forEach((book) => console.log(`${book.id} ${book.name}`))
   })
 }
 
-function createBook(name) {
-  request.post({
-    url: `${BASE_URL}/books`,
-    form: { name }
-  }, (error, response, body) => {
-    if (error) {
-      console.log(error)
-      return
-    }
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      let book
-      try {
-        book = JSON.parse(body)
-        console.log(`${book.id} ${book.name}`)
-        console.log('新增成功')
-        return
-      } catch (error) {
-        console.log(error)
-        return
-      }
-    }
-    console.log('發生問題')
+// 取得 id 為 xxx 的書籍
+const getSpecificBook = (id) => {
+  call({
+    method: 'get',
+    path: `/books/${id}`
+  }, (err, book) => {
+    if (err) return console.log(err.message)
+    console.log(`${book.id} ${book.name}`)
   })
 }
 
-function deleteBook(id) {
-  request(`${BASE_URL}/books/${id}`, (error, response, body) => {
-    if (error) {
-      console.log(error)
-      return
-    }
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      let book
-      try {
-        book = JSON.parse(body)
-      } catch (error) {
-        console.log(error)
-        return
-      }
-      console.log(`${book.id} ${book.name}`)
-      console.log('刪除成功')
-      return
-    }
-    if (response.statusCode === 404) {
-      let parsedBody
-      try {
-        parsedBody = JSON.parse(body)
-        if (!Object.keys(parsedBody).length) {
-          console.log('查無此書')
-          return
-        }
-      } catch (error) {
-      }
-      console.log('發生問題')
-      return
-    }
-    console.log('發生問題')
+// 刪除 id 為 xxx 的書籍
+const deleteBook = (id) => {
+  call({
+    method: 'delete',
+    path: `/books/${id}`
+  }, (err, book) => {
+    if (err) return console.log(err.message)
+    console.log(`${book.id} ${book.name}`)
+    console.log('刪除成功')
   })
 }
 
-function updateBook(id, name) {
-  request.patch({
-    url: `${BASE_URL}/books/${id}`,
-    form: { name }
-  }, (error, response, body) => {
-    if (error) {
-      console.log(error)
-      return
-    }
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      let book
-      try {
-        book = JSON.parse(body)
-      } catch (error) {
-        console.log(error)
-        return
-      }
-      console.log(`${book.id} ${name}`)
-      return
-    }
-    if (response.statusCode === 404) {
-      let parsedBody
-      try {
-        parsedBody = JSON.parse(body)
-        if (!Object.keys(parsedBody).length) {
-          console.log('查無此書')
-          return
-        }
-      } catch (error) {
-      }
-      console.log('發生問題')
-      return
-    }
-    console.log('發生問題')
+// 新增書名為 xxx 的書籍
+const addBook = (name) => {
+  call({
+    method: 'post',
+    path: '/books',
+    body: { name }
+  }, (err, book) => {
+    if (err) return console.log(err.message)
+    console.log(`${book.id} ${book.name}`)
+    console.log('新增成功')
   })
 }
 
+// 更新 id 為 xxx 的書名為 ooo
+const updateBook = (id, name) => {
+  call({
+    method: 'patch',
+    path: `/books/${id}`,
+    body: { name }
+  }, (err, book) => {
+    if (err) return console.log(err.message)
+    console.log(`${book.id} ${book.name}`)
+  })
+}
+
+// 主程式
 function main() {
   switch (process.argv[2]) {
     case 'list':
       getBooks(20)
       break
     case 'read':
-      getBook(process.argv[3])
+      getSpecificBook(process.argv[3])
       break
     case 'delete':
       deleteBook(process.argv[3])
       break
     case 'create':
-      createBook(process.argv[3])
+      addBook(process.argv[3])
       break
     case 'update':
       updateBook(process.argv[3], process.argv[4])
       break
     default:
-      console.log(
-`
-command not found!
-node hw2.js list: 印出前二十本書的 id 與書名
-node hw2.js read 1: 輸出 id 為 1 的書籍
-node hw2.js delete 1: 刪除 id 為 1 的書籍
-node hw2.js create 'I love coding': 新增一本名為 I love coding 的書
-node hw2.js update 1 'new name': 更新 id 為 1 的書名為 new name
-`)
+      console.log('Please use list/read/delete/create/update')
   }
 }
 
--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -1,41 +1,71 @@
-const request = require('request')
-const process = require('process')
+/*
+你的好麻吉小立是一個很愛到處旅遊的人，在前一陣子才靠著便宜的 bug 機票以及特價的商務艙玩遍了許多地方。不過小立一直有個困擾，那就是他希望了解更多跟國家有關的知識，因此他來請你幫忙寫一個搜尋國家資訊的小程式。
 
-const url = `https://restcountries.eu/rest/v2/name/${process.argv[2]}`
-request(
-  url,
-  (error, response, body) => {
-    if (error) {
-      console.log(error)
-    } else if (!error && response.statusCode >= 200 && response.statusCode < 300) {
-      let countries
-      try {
-        countries = JSON.parse(body)
-      } catch (err) {
-        console.log(err)
-        return
-      }
-      console.log('============')
-      for (let i = 0; i < countries.length; i++) {
-        console.log(`國家：${countries[i].name}`)
-        console.log(`首都：${countries[i].capital}`)
-        console.log(`貨幣：${countries[i].currencies[0].code}`)
-        console.log(`國碼：${countries[i].callingCodes[0]}`)
-        console.log('============')
-      }
-    } else if (response.statusCode === 404) {
-      let countries
-      try {
-        countries = JSON.parse(body)
-      } catch (err) {
-        console.log(err)
-        return
-      }
-      if (countries.message === 'Not Found') {
-        console.log('Can not find any country.')
-      }
-    } else {
-      console.log('Something went wrong.')
+這個程式很簡單，只要輸入國家的英文名字，就能夠查詢符合的國家的資訊，會輸出以下幾項：
+
+國家名稱
+首都
+使用的貨幣名稱
+電話國碼
+*/
+const process = require('process')
+const request = require('request')
+const queryString = require('query-string').default
+
+const ACCESS_KEY = '4cbe5c691eac8d66626dd4ca6de22836'
+const BASE_URL = 'https://api.countrylayer.com/v2'
+
+const getCountries = (name) => {
+  const queryParams = queryString.stringify({
+    access_key: ACCESS_KEY,
+    filters: 'name;capital;callingCodes;currencies'
+  })
+  request.get({
+    url: `${BASE_URL}/name/${name}?${queryParams}`
+  }, (err, res, body) => {
+    if (err) {
+      console.log(err)
     }
+
+    if (res.statusCode === 200) {
+      let countries
+      try {
+        countries = JSON.parse(body)
+      } catch (parseErr) {
+        console.log(parseErr)
+        return
+      }
+      console.log('==============')
+      for (const country of countries) {
+        const { name, capital, currencies, callingCodes } = country
+        if (name) console.log(`國家: ${name}`)
+        if (capital) console.log(`首都: ${capital}`)
+        if (Array.isArray(currencies) && currencies.length > 0) {
+          const currencyOutput = []
+          currencies.forEach((ele) => currencyOutput.push(ele))
+          console.log(`貨幣: ${currencyOutput.join(', ')}`)
+        }
+        if (Array.isArray(callingCodes) && callingCodes.length > 0) console.log(`國碼: ${country.callingCodes.join(', ')}`)
+        console.log('==============')
+      }
+    } else if (res.statusCode === 404) {
+      console.log('找不到國家資訊')
+    } else {
+      console.log('發生問題')
+    }
+  })
+}
+
+function main() {
+  const countryName = process.argv[2]
+
+  if (!countryName) {
+    console.log('請輸入國家名稱')
+    console.log('使用方式: node hw3.js <國家名稱>')
+    return
   }
-)
+
+  getCountries(countryName)
+}
+
+main()
